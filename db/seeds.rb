92420a95a922b1028ba5df384de8ddc14e32e9fb
Paul M2B <polo2bodi@gmail.com>
2018-09-09 23:18:42 +0200

    write seeds with 09/09 last pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Simulation au 9 septembre"
+
+Chapter.destroy_all
+DbmPage.destroy_all
+
+chapter_64 = Chapter.create!(
+  number: 64,
+  finished: false,
+  url: "http://www.dragonball-multiverse.com/fr/page-1459.html"
+)
+
+page_1482 = DbmPage.create!(
+  number: 1482,
+  url: "http://www.dragonball-multiverse.com/fr/page-1482.html",
+  first_page: false,
+  chapter: chapter_64
+)
+
+puts "#{Chapter.count} chapter created"
+puts "#{DbmPage.count} page created"
