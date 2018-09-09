498f12c36810916ea0f8f82535b8201f4059c686
Paul M2B <polo2bodi@gmail.com>
2018-09-09 22:10:14 +0200

    create chapters controller

--- db/migrate/20180909193522_create_chapters.rb
+++ db/migrate/20180909193522_create_chapters.rb
@@ -2,10 +2,11 @@ class CreateChapters < ActiveRecord::Migration[5.2]
   def change
     create_table :chapters do |t|
       t.integer :num
-      t.boolean :finished
+      t.boolean :finished, default: false
       t.string :url
-
       t.timestamps
     end
+
+    change_column_null :chapters, :finished, false, false
   end
 end
