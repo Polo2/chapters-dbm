fd3a663e699d9c18fb3ffbe08188028b932888f0
Paul M2B <polo2bodi@gmail.com>
2018-09-26 00:27:24 +0200

    seeds got every page and chapter

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,18 @@ def create_related_chapter(element)
   Chapter.create!( number: number, title: title, finished: true )
 end
 
-def create_page(params)
+def create_related_page(params)
+end
+
+
+def dbm_page_url(number)
+  "http://www.dragonball-multiverse.com/fr/page-#{number}.html"
 end
 
 def initialize_elements(url)
   html_file = open(url).read
   html_doc = Nokogiri::HTML(html_file)
+  html_doc.search('.chfanfic').remove
   @elements = html_doc.search('.chapters')
   return "initialize : done"
 end
@@ -18,40 +24,34 @@ end
 puts "Get all DBM chapters"
 puts "25 septembre"
 
-dbm_url = "http://www.dragonball-multiverse.com/fr/chapters.html"
+dbm_url_chapters = "http://www.dragonball-multiverse.com/fr/chapters.html"
 
-initialize_elements(dbm_url)
+initialize_elements(dbm_url_chapters)
 
 Chapter.destroy_all
 DbmPage.destroy_all
 
-@elements.each { |e| create_related_chapter(e) unless e.attribute('ch').nil? }
+@elements.each do |e|
+  chapter = create_related_chapter(e) unless e.attribute('ch').nil?
+  e.css('a').each do |item|
+    DbmPage.create!(
+      number: item.text.to_i,
+      url: dbm_page_url(item.text.to_i),
+      chapter: chapter,
+    ) unless item.text.empty?
+  end unless chapter.nil?
 
+end
 
-chapter_63 = Chapter.where(number: 63).first
-chapter_63.update!(
-  img_url: "http://www.dragonball-multiverse.com/fr/pages/final/1434.jpg"
-)
-
-# HACK : img urls
+# # HACK : img urls
 chapter_64 = Chapter.where(number: 64).first
-
 chapter_64.update!(
+  img_url: "http://www.dragonball-multiverse.com/fr/pages/final/1459.jpg"
+)
+chapter_65 = Chapter.where(number: 65).first
+chapter_65.update!(
   finished: false,
-  img_url: "http://www.dragonball-multiverse.com/fr/pages/final/1459.jpg",
-)
-
-# HACK : 2 pages
-page_1437 = DbmPage.create!(
-  number: 1437,
-  url: "http://www.dragonball-multiverse.com/fr/page-1437.html",
-  chapter: chapter_63
-)
-
-page_1482 = DbmPage.create!(
-  number: 1482,
-  url: "http://www.dragonball-multiverse.com/fr/page-1482.html",
-  chapter: chapter_64
+  img_url: "http://www.dragonball-multiverse.com/fr/pages/final/1488.jpg",
 )
 
 puts "#{Chapter.count} chapitre créés"
