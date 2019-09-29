236a042ea82d5cf3148b913fbe66e9d2603e41e5
Paul M2B <polo2bodi@gmail.com>
2019-09-29 16:59:24 +0200

    Limit application to chapters page
    Remove a lot of dead code, to limitate navigation to
    only one page : chapters !

--- app/controllers/dbm_pages_controller.rb
+++ app/controllers/dbm_pages_controller.rb
@@ -1,15 +1,4 @@
 class DbmPagesController < ApplicationController
 skip_before_action :authenticate_user!
 
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
-  resources :chapters do
-    resources :dbm_pages
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'chapters#index'
 end
